4c7db9df03b339a435a0c9e547c957fd4b236969
root <Crib1212.igwepatrickchinedu@gmail.com>
2022-10-29 08:39:59 -0700

    task 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
- *main - Entry print a line of code using puts
+ * main -Print a line of code using puts
  *
  * Return: 0
 */
-
 int main(void)
 {
-	puts("/"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
