7b4637d2b0d60e0fc75125462c15902686243098
Crib1212 <igwepatrickchinedu@gmail.com>
2023-01-02 17:08:06 -0800

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
-#include"main.h"
+#include "main.h"
 
 /**
- * _isupper - uppercase letters
- * @c: character to check
+ * _isupper - uppercase letters
+ * @c: character to check
  *
- * Return: 0 or 1
+ * Return: 0 or 1
  */
-int _isupper(int c);
+int _isupper(int c)
 
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1)
+	if (c >= 'A' && c <= 'Z')
+		return (1);
 	else
-		return (0)
+		return (0);
 
 }
