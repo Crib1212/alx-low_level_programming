5b042ea0d6423b29dca421ab4cb6fffb0450279b
Crib1212 <igwepatrickchinedu@gmail.com>
2023-03-23 15:34:08 -0700

    0x0D-preprocessor

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,10 +1,6 @@
-#ifndef _0_OBJECT_LIKE_MARCO_H_ 
-#define _0_OBJECT_LIKE_MARCO_H_ 
-  
-#define SIZE 1024 
-  
+#ifndef _0_OBJECT_LIKE_MARCO_H_
+#define _0_OBJECT_LIKE_MARCO_H_
+
+#define SIZE 1024
+
 #endif
-
-
-
-
